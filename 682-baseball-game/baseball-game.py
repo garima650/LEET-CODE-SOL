31771b2fbb5ea806126109df712965e3c0d1d712
Garima <garimaag36@gmail.com>
2026-09-08 21:31:41 +0530

    Time: 2 ms (55.46%) | Memory: 12.5 MB (45.25%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,21 +1,18 @@
-
-
-class Solution:
+class Solution(object):
     def calPoints(self, operations):
-        rec = []
-        
-        for op in operations:
-            if op == "+":
-                # Add the sum of the last two scores
-                rec.append(rec[-1] + rec[-2])
-            elif op == "D":
-                # Double the last score
-                rec.append(2 * rec[-1])
-            elif op == "C":
-                # Invalidate/remove the last score
-                rec.pop()
+        ops=[]
+        for i in operations:
+
+            if i =="D":
+                ops.append(2*ops[-1])
+            elif i=="C":
+                ops.pop()
+            elif i=="+":
+                ops.append(ops[-1]+ops[-2])
             else:
-                # Value is an integer string, convert and add it
-                rec.append(int(op))
-                
-        return sum(rec)
+                ops.append(int(i))
+
+        return sum(ops)
+        
+
+        
