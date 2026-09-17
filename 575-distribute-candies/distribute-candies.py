7c66740645ccbbf8b63130f284cafd8bab80ba48
Garima <garimaag36@gmail.com>
2026-09-17 22:27:59 +0530

    Time: 24 ms (39.74%) | Memory: 14.2 MB (40.39%) - LeetSync

--- 575-distribute-candies/distribute-candies.py
+++ 575-distribute-candies/distribute-candies.py
@@ -1,13 +1,9 @@
 class Solution(object):
     def distributeCandies(self, candyType):
-        """
-        :type candyType: List[int]
-        :rtype: int
-        """
-        x=set(candyType)
-        if(len(x)==1):
-            return(1)
-        elif(len(x)<(len(candyType)/2)):
-            return(len(x))
-        else:
-            return(len(candyType)/2)
+        
+        n = len(candyType)
+        
+        unique_types = len(set(candyType))
+        
+        return min(unique_types, n // 2)
+        
